bbc392b7986145b448ef08741a2fef60de21c170
Minerzk <scuzk373x@gmail.com>
2018-10-03 11:08:52 +0800

    添加单例懒汉式

--- algorithm/Singleton.cpp
+++ algorithm/Singleton.cpp
@@ -19,3 +19,18 @@ private:
 Singleton_Hungry Singleton_Hungry::m_data;
 
 // 懒汉式
+class Singleton_Lazy
+{
+public:
+    static Singleton_Lazy* getInstance()
+    {
+        if(!m_data) 
+            m_data = new Singleton_Lazy();
+        return m_data;
+    }
+private:
+    static Singleton_Lazy* m_data; //static data member 在类中声明，在类外定义
+    Singleton_Lazy(){}
+    ~Singleton_Lazy(){}
+};
+Singleton_Lazy* Singleton_Lazy::m_data = nullptr;
